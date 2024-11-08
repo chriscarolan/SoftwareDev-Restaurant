eac94e04df5730e066f93174edf218a83127ee00
chriscarolan <89954045+chriscarolan@users.noreply.github.com>
2024-11-07 19:19:11 -0500

    Basic layout of website

--- js_rest.js
+++ js_rest.js
@@ -1,1 +1,31 @@
-//creating a restaurant
+const MenuButton = document.getElementById("menu");
+
+MenuButton.addEventListener("click", function(e) {
+    //const mystery = pickMystery();
+    //const revealMessage = revealMystery(mystery);
+    //mysteryMessage.textContent = revealMessage; 
+});
+
+const AboutButton = document.getElementById("about");
+
+AboutButton.addEventListener("click", function(e) {
+    //const mystery = pickMystery();
+    //const revealMessage = revealMystery(mystery);
+    //mysteryMessage.textContent = revealMessage; 
+});
+
+const ContactButton = document.getElementById("contact");
+
+ContactButton.addEventListener("click", function(e) {
+    //const mystery = pickMystery();
+    //const revealMessage = revealMystery(mystery);
+    //mysteryMessage.textContent = revealMessage; 
+});
+
+const ReservationButton = document.getElementById("reserve");
+
+ReservationButton.addEventListener("click", function(e) {
+    //const mystery = pickMystery();
+    //const revealMessage = revealMystery(mystery);
+    //mysteryMessage.textContent = revealMessage; 
+});
